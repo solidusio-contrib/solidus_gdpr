030e71d5c6603357de9d6e0152d68a7d109bfd86
Alessandro Desantis <desa.alessandro@gmail.com>
2019-09-02 11:04:04 +0200

    Fix outer local variable shadowing in Zeitwerk configuration

--- lib/solidus_gdpr.rb
+++ lib/solidus_gdpr.rb
@@ -4,7 +4,7 @@ require 'solidus_core'
 require 'zeitwerk'
 require 'zip'
 
-loader = Zeitwerk::Loader.for_gem.tap do |loader|
+zeitwerk = Zeitwerk::Loader.for_gem.tap do |loader|
   loader.ignore("#{__dir__}/solidus_gdpr/factories.rb")
   loader.ignore("#{__dir__}/generators")
 
@@ -31,4 +31,4 @@ module SolidusGdpr
   end
 end
 
-loader.eager_load
+zeitwerk.eager_load
