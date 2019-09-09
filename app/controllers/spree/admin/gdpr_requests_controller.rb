826cffa6cac6b97dfa9bca4a400e372a5174712e
Alessandro Desantis <desa.alessandro@gmail.com>
2019-09-09 12:28:44 +0200

    Use Arel.sql for wrapping literal SQL conditions
    See https://github.com/rails/rails/issues/32995 for more details.

--- app/controllers/spree/admin/gdpr_requests_controller.rb
+++ app/controllers/spree/admin/gdpr_requests_controller.rb
@@ -25,9 +25,12 @@ module Spree
       private
 
       def collection
+        served_at_cond = 'served_at IS NOT NULL'
+        served_at_cond = Arel.sql(served_at_cond) if Arel.respond_to?(:sql)
+
         @collection ||=
           Spree::GdprRequest
-          .order('served_at IS NOT NULL', created_at: :desc)
+          .order(served_at_cond, created_at: :desc)
           .page(params[:page] || 0)
       end
 
